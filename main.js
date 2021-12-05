bd3bb957aff4b830e3497fd89cfcf9d7c2f55a8b
Huann Almeida <huannvictor@gmail.com>
2021-12-04 23:49:27 -0300

    day2: iconpack+ two sections added: about+services

---
+++ main.js
@@ -0,0 +1,18 @@
+// toggle menu
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+
+// click menu links
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
